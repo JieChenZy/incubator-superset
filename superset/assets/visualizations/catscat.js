74a72cb43ad208613f14af6af842a0c9cc451410
Jim Vallandingham <jvallandingham@zymergen.com>
2017-11-14 13:59:58 -0800

    Added comments and tooltip

--- superset/assets/visualizations/catscat.js
+++ superset/assets/visualizations/catscat.js
@@ -1,7 +1,18 @@
 import d3 from 'd3';
+import d3tip from 'd3-tip';
 import { getColorFromScheme } from '../javascripts/modules/colors';
-import './catscat.css';
 
+import './catscat.css';
+import '../stylesheets/d3tip.css';
+
+/**
+ * computedProps
+ * returns derived properties and functions based on input.
+ * Used to conceptually distinguish the 'setting up' of
+ * a chart from the actual 'dom manipulation' to create the chart.
+ * @param {Object} props : Input properties to use to compute
+ * @returns {Object} : Object of computed props.
+ */
 function computedProps(props) {
   const { fd, data, width, height } = props;
 
@@ -14,13 +25,16 @@ function computedProps(props) {
 
   const plotWidth = width - (padding.right + padding.left);
   const plotHeight = height - (padding.top + padding.bottom);
-
+  
+  // api: https://github.com/d3/d3-3.x-api-reference/blob/master/Quantitative-Scales.md#linear
   const yMin = d3.min(data, d => d3.min(d.values, v => v.y));
   const yMax = d3.max(data, d => d3.max(d.values, v => v.y));
   const yScale = d3.scale.linear()
     .domain([yMin, yMax])
     .range([plotHeight, 0]);
 
+  // as this is a categorical scatterplot, our x scale will be ordinal. 
+  // api: https://github.com/d3/d3-3.x-api-reference/blob/master/Ordinal-Scales.md#ordinal_rangePoints
   const xScale = d3.scale.ordinal()
     .domain(d3.range(data.length))
     .rangePoints([0, plotWidth]);
@@ -31,6 +45,7 @@ function computedProps(props) {
     .domain(shapeValues)
     .range(d3.svg.symbolTypes);
 
+  // Currently unused, but we should use it in the yAxis creation below
   const yFormat = d3.format(fd.y_axis_format);
 
   return {
@@ -44,26 +59,55 @@ function computedProps(props) {
   };
 }
 
-
+/**
+ * Main Visualization method.
+ * Takes the slice and json objects and
+ * modifies DOM to create chart.
+ * 
+ * @param {Object} slice 
+ * @param {Object} json 
+ */
 function scatCatViz(slice, json) {
+  // this removes everything that might already
+  // be in the vis's container.
+  // There is probably a more elegant way to do this.
+  slice.container.html('');
+
+  // D3 selector of our outer container
+  // api: https://github.com/d3/d3-3.x-api-reference/blob/master/Selections.md#d3_select
   const div = d3.select(slice.selector);
   const fd = slice.formData;
 
   const width = slice.width();
   const height = slice.height();
 
+  // Currently the data is nested so we can 
+  // easily access both the actual data as well
+  // as the yLines array. This could be simplified,
+  // or renamed.
   const data = json.data.data;
+
+  // data is an array of objects, with each object having a 'key' and 'values' attribute:
+  // [{key: 'group 1', values: [{y: 20, ...}, {}...]}]
+
   const yLines = json.data.yLines;
 
+  // call to computedProps to setup our padding and scales
   const { padding, plotHeight, plotWidth, yScale, xScale, shapeScale } = computedProps({ fd, data, width, height });
 
+  // append a new SVG element to our div container
+  // api: https://github.com/d3/d3-3.x-api-reference/blob/master/Selections.md#append
   const svg = div.append('svg')
     .attr('width', width)
     .attr('height', height);
 
+  // This g is where our chart will be drawn. We move it over to make room for
+  // the padding
+  // api: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/transform
   const g = svg.append('g')
     .attr('transform', `translate(${padding.left}, ${padding.top})`);
 
+  // axes group elements
   const yAxis = g.append('g')
     .attr('class', 'y axis');
 
@@ -71,13 +115,35 @@ function scatCatViz(slice, json) {
     .attr('transform', `translate(${0},${plotHeight + (padding.top / 2)})`)
     .attr('class', 'x axis');
 
+  // how you add an axis to a chart in d3.
+  // api: https://github.com/d3/d3-3.x-api-reference/blob/master/SVG-Axes.md#axis
   yAxis
     .call(d3.svg.axis().scale(yScale).orient('left'));
 
   xAxis
     .call(d3.svg.axis().scale(xScale).orient('bottom'));
 
+  // Super quick tooltip using existing tooltip library
+  // api: https://github.com/Caged/d3-tip
+  const renderTooltip = (d) => {
+    return `
+      <div>
+        <span>${d.group}: </span>
+        <strong>${d.y}</strong>
+      </div>
+    `;
+  };
 
+  const tip = d3tip()
+    .attr('class', 'd3-tip')
+    .direction('n')
+    .offset([-5, 0])
+    .html(renderTooltip);
+  svg.call(tip);
+
+
+  // data binding and enter selection
+  // for outer data array
   const band = g.selectAll('.band')
     .data(data)
     .enter()
@@ -85,14 +151,25 @@ function scatCatViz(slice, json) {
     .classed('band', true)
     .attr('transform', (d, i) => `translate(${xScale(i)}, ${0})`);
 
+  // data binding and enter selection
+  // for each .values array of our data
   band.selectAll('.point')
     .data(d => d.values)
     .enter()
     .append('path')
     .classed('point', true)
     .attr('d', d3.svg.symbol().type(d => shapeScale(d.shape)))
-    .attr('transform', d => `translate(${0}, ${yScale(d.y)})`);
+    .attr('transform', d => `translate(${0}, ${yScale(d.y)})`)
+    .on('mouseover', function (d) {
+      tip.show(d);
+    })
+    .on('mouseout', function (d) {
+      tip.hide(d);
+    });
 
+  // d3 symbols: https://github.com/d3/d3-3.x-api-reference/blob/master/SVG-Shapes.md#symbol
+
+  // add lines if present
   if (yLines) {
     g.selectAll('.line')
       .data(yLines)
@@ -104,7 +181,7 @@ function scatCatViz(slice, json) {
       .attr('x1', 0)
       .attr('x2', plotWidth)
       .attr('y1', d => yScale(d))
-      .attr('y2', d => yScale(d))
+      .attr('y2', d => yScale(d));
   }
 }
 
