90874c64780833a8df5c8404038320b9588e3867
Jim Vallandingham <jvallandingham@zymergen.com>
2017-12-13 13:36:05 -0800

    Sort values by colorBy

--- superset/assets/visualizations/catscat.js
+++ superset/assets/visualizations/catscat.js
@@ -6,6 +6,25 @@ import './catscat.css';
 import '../stylesheets/d3tip.css';
 
 /**
+ * For a given array, return the value associated with attr from
+ * the first element in the array. If the array is empty, return
+ * the defaultValue.
+ *
+ * @param {Array} array Array of values to pick from
+ * @param {String} attr Attribute to select
+ * @param {Any} defaultValue Value to use if array is empty
+ */
+function pickFirst(array, attr, defaultValue = null) {
+  return array.length > 0 ? array[0][attr] : defaultValue;
+}
+
+
+function sortData(data, sortBy) {
+  data.sort((a, b) => +pickFirst(a.values, sortBy) - +pickFirst(b.values, sortBy));
+  return data;
+}
+
+/**
  * computedProps
  * returns derived properties and functions based on input.
  * Used to conceptually distinguish the 'setting up' of
@@ -25,9 +44,11 @@ function computedProps(props) {
   const plotWidth = width - (padding.right + padding.left);
   const plotHeight = height - (padding.top + padding.bottom);
 
+  const sortedData = sortData(data, 'color');
+
   // api: https://github.com/d3/d3-3.x-api-reference/blob/master/Quantitative-Scales.md#linear
-  const yMin = d3.min(data, d => d3.min(d.values, v => v.y));
-  const yMax = d3.max(data, d => d3.max(d.values, v => v.y));
+  const yMin = d3.min(sortedData, d => d3.min(d.values, v => v.y));
+  const yMax = d3.max(sortedData, d => d3.max(d.values, v => v.y));
   const yScale = d3.scale.linear()
     .domain([yMin, yMax])
     .range([plotHeight, 0]);
@@ -35,11 +56,10 @@ function computedProps(props) {
   // as this is a categorical scatterplot, our x scale will be ordinal. 
   // api: https://github.com/d3/d3-3.x-api-reference/blob/master/Ordinal-Scales.md#ordinal_rangePoints
   const xScale = d3.scale.ordinal()
-    .domain(d3.range(data.length))
+    .domain(d3.range(sortedData.length))
     .rangePoints([0, plotWidth]);
 
-  // Currently colorScale is unused for plot, since superset already has getColorFromScheme. 
-  // Might be useful for creating legend.
+  // color scale
   const colorValuesAll = data.map(d => d.values.map(v => v.color));
   const colorValues = d3.set([].concat(...colorValuesAll)).values();
   const colorScale = d3.scale.ordinal()
@@ -55,8 +75,11 @@ function computedProps(props) {
   // Currently unused, but we should use it in the yAxis creation below
   const yFormat = d3.format(fd.y_axis_format);
 
+  const displayXAxis = false;
+
   return {
     padding,
+    sortedData,
     plotWidth,
     plotHeight,
     yScale,
@@ -64,8 +87,10 @@ function computedProps(props) {
     colorScale,
     shapeScale,
     yFormat,
+    displayXAxis,
   };
 }
+
 
 /**
  * Main Visualization method.
@@ -100,7 +125,7 @@ function scatCatViz(slice, json) {
   const yLines = json.data.yLines;
 
   // call to computedProps to setup our padding and scales
-  const { padding, plotHeight, plotWidth, yScale, xScale, colorScale, shapeScale } = computedProps({ fd, data, width, height });
+  const { padding, sortedData, plotHeight, plotWidth, yScale, xScale, displayXAxis, colorScale, shapeScale } = computedProps({ fd, data, width, height });
   // append a new SVG element to our div container
   // api: https://github.com/d3/d3-3.x-api-reference/blob/master/Selections.md#append
   const svg = div.append('svg')
@@ -118,16 +143,21 @@ function scatCatViz(slice, json) {
     .attr('class', 'y axis');
 
   const xAxis = g.append('g')
-    .attr('transform', `translate(${0},${plotHeight + (padding.top / 2)})`)
-    .attr('class', 'x axis');
+      .attr('transform', `translate(${0},${plotHeight + (padding.top / 2)})`)
+      .attr('class', 'x axis');
+
+  const legend = g.append('g')
+    .attr('class', 'legend');
 
   // how you add an axis to a chart in d3.
   // api: https://github.com/d3/d3-3.x-api-reference/blob/master/SVG-Axes.md#axis
   yAxis
     .call(d3.svg.axis().scale(yScale).orient('left'));
 
-  xAxis
-    .call(d3.svg.axis().scale(xScale).orient('bottom'));
+  if (displayXAxis) {
+    xAxis
+      .call(d3.svg.axis().scale(xScale).orient('bottom'));
+  }
 
   // Super quick tooltip using existing tooltip library
   // api: https://github.com/Caged/d3-tip
@@ -153,7 +183,7 @@ function scatCatViz(slice, json) {
   // data binding and enter selection
   // for outer data array
   const band = g.selectAll('.band')
-    .data(data)
+    .data(sortedData)
     .enter()
     .append('g')
     .classed('band', true)
